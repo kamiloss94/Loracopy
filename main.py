9420b9009cf317a5e5c4e53b2e297a2d11830d84
kamil.marchewka <kamil.marchewka@apator.com>
2023-01-27 20:38:06 +0100

    Updated unicorn serving address config

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
                         "Raw_payload" : f"{payload}"}
 
 
-        uvicorn.run(app, host="0.0.0.0", port=8000)
+        uvicorn.run(app, host="127.0.0.1", port=8000)
         """
                      
         for obj in meters:
